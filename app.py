02ecd220fff2090c5013e74c05ac9e212b9d9890
pakshahsin1 <pakshahsin1@yahoo.com>
2024-07-21 19:04:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,3 @@ def home():
     # return "<p>Hello, World!</p>"
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
